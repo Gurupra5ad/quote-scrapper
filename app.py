6c7d35a84a1a52617623051bb257687b8be38fe1
Gurupra5ad <guruprasad2511@gmail.com>
2021-02-28 19:34:55 +0530

    updated the code for errors and wait and also added a video

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 from selenium import webdriver
 
-from pages.quotes_page import QuotesPage
+from pages.quotes_page import QuotesPage, InvalidTagForAuthorError
 
-author = input("Enter the author you 'd like quotes from: ")
-tag =  input("Enter your tag: ")
+try:
+    author = input("Enter the author you 'd like quotes from: ")
+    tag =  input("Enter your tag: ")
 
-chrome = webdriver.Chrome(executable_path="/home/disciple/chromedriver")
-chrome.get("http://quotes.toscrape.com/search.aspx")
-page = QuotesPage(chrome)
+    chrome = webdriver.Chrome(executable_path="/home/disciple/chromedriver")
+    chrome.get("http://quotes.toscrape.com/search.aspx")
+    page = QuotesPage(chrome)
 
-print(page.search_for_quotes(author,tag))
+    print(page.search_for_quotes(author,tag))
+except InvalidTagForAuthorError as e:
+    print(e)
+except Exception as e:
+    print(e)
+    print("An unknown error as occured, Please try again.")
+
